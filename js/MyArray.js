7c7125b6f04ee857089962a7f73be5aac4fc19f5
morghull <morghull@gmail.com>
2021-10-11 16:41:15 +0300

    refactor for MyArray.js

--- js/MyArray.js
+++ js/MyArray.js
@@ -1,43 +1,24 @@
+'use strict';
 /*logic*/
 function MyArrayProto() {
-  this.push = function () {
-    for (let i = 0; i < arguments.length; i++) {
+  this.push = function (...args) {
+    args.forEach((item) => {
       if (
-        arguments[i] instanceof MyArray ||
-        arguments[i].constructor === Array
+        item instanceof MyArray ||
+        item.constructor === Array
       ) {
-        for (let j = 0; j < arguments[i].length; j++) {
-          this[this.length++] = arguments[i][j];
-        }
-      } else this[this.length++] = arguments[i];
-    }
+        item.forEach((subitem) => {
+          this[this.length++] = subitem;
+        });
+      } else this[this.length++] = item;
+    });
     return this.length;
   };
-  this.unshift = function () {
-    for (let i = arguments.length - 1; i >= 0; i--) {
-      if (
-        arguments[i] instanceof MyArray ||
-        arguments[i].constructor === Array
-      ) {
-        for (let j = arguments[i].length - 1; j >= 0; j--) {
-          let value = arguments[i][j];
-          for (let k = 0; k <= this.length; k++) {
-            const buffer = this[k];
-            this[k] = value;
-            value = buffer;
-          }
-          this.length++;
-        }
-      } else {
-        let value = arguments[i];
-        for (let j = 0; j <= this.length; j++) {
-          const buffer = this[j];
-          this[j] = value;
-          value = buffer;
-        }
-        this.length++;
-      }
-    }
+  this.unshift = function (...args) {
+    this.reverse();
+    args.reverse();
+    this.push(...args);
+    this.reverse();
     return this.length;
   };
   this.reverse = function () {
@@ -58,12 +39,12 @@ function MyArrayProto() {
     delete this[this.length];
     return item;
   };
-  this.concat = function () {
+  this.concat = function (...args) {
     const result = new MyArray();
     result.push(this);
-    for (let i = 0; i < arguments.length; i++) {
-      result.push(arguments[i]);
-    }
+    args.forEach((item) => {
+      result.push(item);
+    });
     return result;
   };
   this.slice = function (begin, end = this.length) {
@@ -137,6 +118,18 @@ const labelStyle = 'color:darkcyan;';
 console.log('%c%s', labelStyleBasis, 'Task 1 CONCAT');
 const concatArray1 = new MyArray([1, 2, 3]);
 const concatArray2 = new MyArray(4, 5, 6);
+console.log(
+  '%c%s',
+  labelStyle,
+  'trying to concat array 1:',
+  concatArray1
+);
+console.log(
+  '%c%s',
+  labelStyle,
+  'trying to concat array 2:',
+  concatArray2
+);
 const concatedArray = new MyArray().concat(
   concatArray1,
   concatArray2
@@ -145,6 +138,7 @@ console.log('%c%s', labelStyle, 'concat:', concatedArray);
 
 console.log('%c%s', taskLabelStyle, 'Task 2 REVERSE');
 const reverseArray = new MyArray(1, 2, 3);
+console.log('%c%s', labelStyle, 'before:', reverseArray);
 console.log(
   '%c%s',
   labelStyle,
@@ -154,16 +148,33 @@ console.log(
 
 console.log('%c%s', taskLabelStyle, 'Task 3 PUSH');
 const pushArray = new MyArray(1, 2, 3);
-pushArray.push(4, [5], new MyArray(6));
-console.log('%c%s', 'color:darkcyan;', 'push:', pushArray);
+console.log('%c%s', labelStyle, 'before:', pushArray);
+const tryingToPush = [4, [5], new MyArray(6)];
+console.log(
+  '%c%s',
+  labelStyle,
+  'trying to push:',
+  tryingToPush
+);
+pushArray.push(...tryingToPush);
+console.log('%c%s', labelStyle, 'after push:', pushArray);
 
 console.log('%c%s', taskLabelStyle, 'Task 4 UNSHIFT');
 const unshiftArray = new MyArray(1, 2, 3);
-unshiftArray.unshift(4, [5], new MyArray(6));
+console.log('%c%s', labelStyle, 'before:', unshiftArray);
+const tryingToUnshift = [4, [5], new MyArray(6)];
+console.log(
+  '%c%s',
+  labelStyle,
+  'trying to unshift:',
+  tryingToUnshift
+);
+unshiftArray.unshift(...tryingToUnshift);
 console.log('%c%s', labelStyle, 'unshift:', unshiftArray);
 
 console.log('%c%s', taskLabelStyle, 'Task 5 SHIFT');
 const shiftArray = new MyArray('js', 'css', 'html');
+console.log('%c%s', labelStyle, 'before:', shiftArray);
 const shiftedItem = shiftArray.shift();
 console.log(
   '%c%s',
@@ -175,6 +186,7 @@ console.log('%c%s', labelStyle, 'shifted:', shiftArray);
 
 console.log('%c%s', taskLabelStyle, 'Task 5 POP');
 const popArray = new MyArray('js', 'css', 'html');
+console.log('%c%s', labelStyle, 'before:', popArray);
 const popedItem = popArray.pop();
 console.log(
   '%c%s',
@@ -188,12 +200,7 @@ console.log('%c%s', taskLabelStyle, 'Task 5 SLICE');
 const sliceArray = new MyArray(1, 2, 3, 4, 5);
 const slicedArray1 = sliceArray.slice(1, 3);
 const slicedArray2 = sliceArray.slice(4, 5);
-console.log(
-  '%c%s',
-  labelStyle,
-  'slice array:',
-  sliceArray
-);
+console.log('%c%s', labelStyle, 'slice array:', sliceArray);
 console.log(
   '%c%s',
   labelStyle,
